0d974d5c04fc47be24840c45d3469c04979517cb
syara19 <syahrulrajab@gmail.com>
2023-09-11 16:40:31 +0700

    bug edit book, logout

--- src/app/books/create/page.js
+++ src/app/books/create/page.js
@@ -1,6 +1,29 @@
 import { BookForm } from "@/app/components/book/BookForm";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function CreateBook() {
+    const [book, setBook] = useState(null);
+    const router = useRouter();
+    const { id } = router.query;
+    console.log(id)
+
+    useEffect(()=>{
+        const fetchBook = async ()=>{
+            try {
+                const res = await fetch(`http://localhost:3000/api/books/${id}`,{
+                    method: "GET",
+                });
+                console.log(res.book)
+                // setBook (res)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchBook()
+    },[id])
+
+
     console.log("test")
     return(
         <div>
