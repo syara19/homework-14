16fefdb3b78a85228213d68975e2921d7bd9b936
syara19 <syahrulrajab@gmail.com>
2023-09-11 09:15:13 +0700

    repair ui login/register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -25,63 +25,11 @@ export default function Register() {
   };
 
   return (
-    // <div className="hero min-h-screen">
-    //   <div className="hero-content flex-col lg:flex-row-reverse ">
-    //     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-    //       <div className="card-body">
-    //         <form onSubmit={submitHandler}>
-    //           <div className="form-control">
-    //             <label className="label" htmlFor="name">
-    //               <span className="label-text">Name</span>
-    //             </label>
-    //             <input
-    //               className="input input-bordered "
-    //               placeholder="name"
-    //               type="text"
-    //               name="name"
-    //             />
-    //           </div>
-    //           <div className="form-control">
-    //             <label className="label" htmlFor="email">
-    //               <span className="label-text">Email</span>
-    //             </label>
-    //             <input
-    //               className="input input-bordered "
-    //               placeholder="email"
-    //               type="email"
-    //               name="email"
-    //             />
-    //           </div>
-    //           <div className="form-control">
-    //             <label className="label" htmlFor="password">
-    //               <span className="label-text">Password</span>
-    //             </label>
-    //             <input
-    //               className="input input-bordered "
-    //               placeholder="password"
-    //               type="password"
-    //               name="password"
-    //             />
-    //           </div>
-    //           <label className="label">
-    //             <Link
-    //               href={"/login"}
-    //               className="label-text-alt link link-hover"
-    //             >
-    //               Have an account?
-    //             </Link>
-    //           </label>
-    //           <div className="form-control mt-6">
-    //             <button className="btn btn-primary">Login</button>
-    //           </div>
-    //         </form>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="max-w-md w-full mx-auto p-8 bg-white rounded-lg shadow-lg">
-        <h2 className="text-3xl  text-slate-800 font-bold text-center mb-6">Register</h2>
+        <h2 className="text-3xl  text-slate-800 font-bold text-center mb-6">
+          Register
+        </h2>
         <form onSubmit={submitHandler}>
           <div className="mb-4">
             <label htmlFor="name" className="block text-gray-700">
